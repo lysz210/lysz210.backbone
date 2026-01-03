69e7b8f3cb868f7e4f425385aa4ba37048a01582
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-03 18:18:50 +0100

    first commit

---
+++ provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  # Configurazione per Terraform Cloud
+  cloud {
+    organization = "lysz210"
+
+    workspaces {
+      name = "lysz210-backbone"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Provider principale (es. Irlanda)
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Provider secondario per risorse globali (Certificati e DNSSEC)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
