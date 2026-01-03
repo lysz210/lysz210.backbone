fa82ba000e924efa5ce55e9c4b3bfe2caa6f17c7
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-03 23:31:05 +0100

    update

--- dns.tf
+++ dns.tf
@@ -1,62 +1,62 @@
-# # 1. Creazione della Hosted Zone principale
-# resource "aws_route53_zone" "main" {
-#   name          = "lysz210.name"
-#   comment       = "Managed by Terraform - Core Infrastructure"
-#   force_destroy = false
-# }
+# 1. Creazione della Hosted Zone principale
+resource "aws_route53_zone" "main" {
+  name          = "lysz210.name"
+  comment       = "Managed by Terraform - Core Infrastructure"
+  force_destroy = false
+}
 
-# # 2. Record TXT per Keybase (Verifica Sito)
-# resource "aws_route53_record" "keybase_verification" {
-#   zone_id = aws_route53_zone.main.zone_id
-#   name    = "lysz210.name"
-#   type    = "TXT"
-#   ttl     = 172800
-#   records = ["keybase-site-verification=CSIcI9LwdT2ZCbeyPqVAEIL0Z65TSaByfWoVkeku5Gc"]
-# }
+# 2. Record TXT per Keybase (Verifica Sito)
+resource "aws_route53_record" "keybase_verification" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "lysz210.name"
+  type    = "TXT"
+  ttl     = 172800
+  records = ["keybase-site-verification=CSIcI9LwdT2ZCbeyPqVAEIL0Z65TSaByfWoVkeku5Gc"]
+}
 
-# # 4. Certificato Wildcard (Gestito centralmente)
-# resource "aws_acm_certificate" "wildcard" {
-#   provider                  = aws.us_east_1 # Deve essere in us-east-1 per CloudFront
-#   domain_name               = "*.lysz210.name"
-#   subject_alternative_names = ["lysz210.name"]
-#   validation_method         = "DNS"
+# 4. Certificato Wildcard (Gestito centralmente)
+resource "aws_acm_certificate" "wildcard" {
+  provider                  = aws.us_east_1 # Deve essere in us-east-1 per CloudFront
+  domain_name               = "*.lysz210.name"
+  subject_alternative_names = ["lysz210.name"]
+  validation_method         = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   tags = {
-#     Name = "wildcard-lysz210-name"
-#   }
-# }
+  tags = {
+    Name = "wildcard-lysz210-name"
+  }
+}
 
-# # Record DNS per la validazione automatica del certificato
-# resource "aws_route53_record" "cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+# Record DNS per la validazione automatica del certificato
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = aws_route53_zone.main.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
 
-# # 5. Export dei parametri su SSM (Per i futuri Microfrontend)
-# resource "aws_ssm_parameter" "zone_id" {
-#   name  = "/infra/core/route53_zone_id"
-#   type  = "String"
-#   value = aws_route53_zone.main.zone_id
-# }
+# 5. Export dei parametri su SSM (Per i futuri Microfrontend)
+resource "aws_ssm_parameter" "zone_id" {
+  name  = "/infra/core/route53_zone_id"
+  type  = "String"
+  value = aws_route53_zone.main.zone_id
+}
 
-# resource "aws_ssm_parameter" "wildcard_cert_arn" {
-#   name  = "/infra/core/wildcard_cert_arn"
-#   type  = "String"
-#   value = aws_acm_certificate.wildcard.arn
-# }
+resource "aws_ssm_parameter" "wildcard_cert_arn" {
+  name  = "/infra/core/wildcard_cert_arn"
+  type  = "String"
+  value = aws_acm_certificate.wildcard.arn
+}
