233a4d3955eb5a3d3735d7de3f2d75fe625f46fb
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 00:12:05 +0100

    update

--- dns.tf
+++ dns.tf
@@ -59,58 +59,58 @@ resource "aws_ssm_parameter" "wildcard_cert_arn" {
 
 # --- DNSSEC SECTION ---
 
-# resource "aws_kms_key" "dnssec" {
-#   provider                 = aws.us_east_1
-#   customer_master_key_spec = "ECC_NIST_P256"
-#   key_usage                = "SIGN_VERIFY"
-#   description              = "KMS Key for DNSSEC - lysz210.me"
+resource "aws_kms_key" "dnssec" {
+  provider                 = aws.us_east_1
+  customer_master_key_spec = "ECC_NIST_P256"
+  key_usage                = "SIGN_VERIFY"
+  description              = "KMS Key for DNSSEC - lysz210.me"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid    = "Enable IAM User Permissions"
-#         Effect = "Allow"
-#         Principal = { AWS = "*" }
-#         Action   = "kms:*"
-#         Resource = "*"
-#       },
-#       {
-#         Sid    = "Allow Route 53 DNSSEC Service"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "api-dnssec.route53.amazonaws.com" # FIX: Nome servizio corretto
-#         }
-#         Action = [
-#           "kms:DescribeKey",
-#           "kms:GetPublicKey",
-#           "kms:Sign"
-#         ]
-#         Resource = "*"
-#       },
-#       {
-#         Sid    = "Allow Route 53 Create Grant" # FIX: Necessario per DNSSEC
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "api-dnssec.route53.amazonaws.com"
-#         }
-#         Action   = "kms:CreateGrant"
-#         Resource = "*"
-#         Condition = {
-#           Bool = { "kms:GrantIsForAWSResource" = "true" }
-#         }
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = { AWS = "*" }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow Route 53 DNSSEC Service"
+        Effect = "Allow"
+        Principal = {
+          Service = "api-dnssec.route53.amazonaws.com" # FIX: Nome servizio corretto
+        }
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow Route 53 Create Grant" # FIX: Necessario per DNSSEC
+        Effect = "Allow"
+        Principal = {
+          Service = "api-dnssec.route53.amazonaws.com"
+        }
+        Action   = "kms:CreateGrant"
+        Resource = "*"
+        Condition = {
+          Bool = { "kms:GrantIsForAWSResource" = "true" }
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_route53_key_signing_key" "main" {
-#   hosted_zone_id             = aws_route53_zone.main.zone_id
-#   key_management_service_arn = aws_kms_key.dnssec.arn
-#   name                       = "lysz210-ksk"
-# }
+resource "aws_route53_key_signing_key" "main" {
+  hosted_zone_id             = aws_route53_zone.main.zone_id
+  key_management_service_arn = aws_kms_key.dnssec.arn
+  name                       = "lysz210-ksk"
+}
 
-# resource "aws_route53_hosted_zone_dnssec" "main" {
-#   depends_on     = [aws_route53_key_signing_key.main]
-#   hosted_zone_id = aws_route53_key_signing_key.main.hosted_zone_id
-# }
+resource "aws_route53_hosted_zone_dnssec" "main" {
+  depends_on     = [aws_route53_key_signing_key.main]
+  hosted_zone_id = aws_route53_key_signing_key.main.hosted_zone_id
+}
