c744ed8e17f5de439a1f455901a620852b369ab0
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 18:09:26 +0100

    Reapply "update"
    This reverts commit e51ab7615858d0990fb974e531e635747658a3c1.

--- modules/host/github.tf
+++ modules/host/github.tf
@@ -1,33 +1,33 @@
-resource "github_repository" "lysz210_host" {
-  name        = "${var.github_repo}"
-  description = "Host Application for lysz210.me"
-  auto_init = true
-}
+# resource "github_repository" "lysz210_host" {
+#   name        = "${var.github_repo}"
+#   description = "Host Application for lysz210.me"
+#   auto_init = true
+# }
 
-resource "github_branch" "develop" {
-  repository = github_repository.lysz210_host.name
-  branch     = "develop"
-  depends_on = [ github_repository.lysz210_host ]
-}
-resource "github_branch_default" "default_branch" {
-  repository = github_repository.lysz210_host.name
-  branch     = github_branch.develop.branch
-}
-resource "github_actions_variable" "iam_role_arn" {
-  repository    = github_repository.lysz210_host.name
-  variable_name = "AWS_ROLE_ARN"
-  value         = aws_iam_role.github_actions_role.arn
-}
+# resource "github_branch" "develop" {
+#   repository = github_repository.lysz210_host.name
+#   branch     = "develop"
+#   depends_on = [ github_repository.lysz210_host ]
+# }
+# resource "github_branch_default" "default_branch" {
+#   repository = github_repository.lysz210_host.name
+#   branch     = github_branch.develop.branch
+# }
+# resource "github_actions_variable" "iam_role_arn" {
+#   repository    = github_repository.lysz210_host.name
+#   variable_name = "AWS_ROLE_ARN"
+#   value         = aws_iam_role.github_actions_role.arn
+# }
 
-resource "github_actions_variable" "s3_bucket" {
-  repository    = github_repository.lysz210_host.name
-  variable_name = "S3_BUCKET_NAME"
-  value         = aws_s3_bucket.lysz210_host_storage.id
-}
+# resource "github_actions_variable" "s3_bucket" {
+#   repository    = github_repository.lysz210_host.name
+#   variable_name = "S3_BUCKET_NAME"
+#   value         = aws_s3_bucket.lysz210_host_storage.id
+# }
 
-# Crea la variabile per l'ID di CloudFront
-resource "github_actions_variable" "cloudfront_id" {
-  repository    = github_repository.lysz210_host.name
-  variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
-  value         = aws_cloudfront_distribution.lysz210_host_distribution.id
-}
+# # Crea la variabile per l'ID di CloudFront
+# resource "github_actions_variable" "cloudfront_id" {
+#   repository    = github_repository.lysz210_host.name
+#   variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
+#   value         = aws_cloudfront_distribution.lysz210_host_distribution.id
+# }
--- modules/host/outputs.tf
+++ modules/host/outputs.tf
@@ -1,10 +1,10 @@
-output "repo_url" {
-  value = github_repository.lysz210_host.html_url
-}
+# output "repo_url" {
+#   value = github_repository.lysz210_host.html_url
+# }
 
-output "repo_ssh" {
-  value = github_repository.lysz210_host.ssh_clone_url
-}
+# output "repo_ssh" {
+#   value = github_repository.lysz210_host.ssh_clone_url
+# }
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.lysz210_host_storage.id
