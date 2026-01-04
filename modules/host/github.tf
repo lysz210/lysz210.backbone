e05ff9a3e03c1f090dc64ea35b5c89a8b48e605f
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 16:43:14 +0100

    update

--- modules/host/github.tf
+++ modules/host/github.tf
@@ -3,20 +3,20 @@ data "github_repository" "lysz210_host" {
 }
 
 resource "github_actions_variable" "iam_role_arn" {
-  repository    = data.github_repository.lysz210_host.name
+  repository    = data.github_repository.lysz210_host.full_name
   variable_name = "AWS_ROLE_ARN"
   value         = aws_iam_role.github_actions_role.arn
 }
 
-# resource "github_actions_variable" "s3_bucket" {
-#   repository    = data.github_repository.lysz210_host.id
-#   variable_name = "S3_BUCKET_NAME"
-#   value         = aws_s3_bucket.lysz210_host_storage.id
-# }
+resource "github_actions_variable" "s3_bucket" {
+  repository    = data.github_repository.lysz210_host.full_name
+  variable_name = "S3_BUCKET_NAME"
+  value         = aws_s3_bucket.lysz210_host_storage.id
+}
 
-# # Crea la variabile per l'ID di CloudFront
-# resource "github_actions_variable" "cloudfront_id" {
-#   repository    = data.github_repository.lysz210_host.id
-#   variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
-#   value         = aws_cloudfront_distribution.lysz210_host_distribution.id
-# }
+# Crea la variabile per l'ID di CloudFront
+resource "github_actions_variable" "cloudfront_id" {
+  repository    = data.github_repository.lysz210_host.full_name
+  variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
+  value         = aws_cloudfront_distribution.lysz210_host_distribution.id
+}
