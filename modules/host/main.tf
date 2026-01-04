6f81f4b767164662d9f9950bf1ebcfbcc91043b3
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 22:30:58 +0100

    update host redirect

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -46,6 +46,12 @@ resource "aws_cloudfront_distribution" "lysz210_host_distribution" {
   }
 
   custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
     error_code         = 404
     response_code      = 200
     response_page_path = "/index.html"
