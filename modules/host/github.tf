97f27ac2844b11512540bf1ba0d3a80516ee222c
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 16:33:52 +0100

    update

--- modules/host/github.tf
+++ modules/host/github.tf
@@ -3,7 +3,7 @@ data "github_repository" "lysz210_host" {
 }
 
 resource "github_branch_default" "main" {
-  repository = data.github_repository.lysz210_host.name
+  repository = data.github_repository.lysz210_host.id
   branch     = "main"
 }
 
