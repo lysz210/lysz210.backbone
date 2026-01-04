165e0edb2a2baa71dc1bbf5a71663561fc496d7d
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 17:18:20 +0100

    update

--- modules/host/github.tf
+++ modules/host/github.tf
@@ -1,18 +1,4 @@
-resource "github_actions_variable" "iam_role_arn" {
-  repository    = var.github_repo
-  variable_name = "AWS_ROLE_ARN"
-  value         = aws_iam_role.github_actions_role.arn
-}
-
-resource "github_actions_variable" "s3_bucket" {
-  repository    = var.github_repo
-  variable_name = "S3_BUCKET_NAME"
-  value         = aws_s3_bucket.lysz210_host_storage.id
-}
-
-# Crea la variabile per l'ID di CloudFront
-resource "github_actions_variable" "cloudfront_id" {
-  repository    = var.github_repo
-  variable_name = "CLOUDFRONT_DISTRIBUTION_ID"
-  value         = aws_cloudfront_distribution.lysz210_host_distribution.id
+resource "github_repository" "lysz210_host" {
+  name        = "${var.github_repo}"
+  description = "Host Application for lysz210.me"
 }
