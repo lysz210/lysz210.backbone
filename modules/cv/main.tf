885ce8a438a49c328c49094aadc63aeba6c6310e
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 21:01:23 +0100

    add cv lambda

--- modules/cv/main.tf
+++ modules/cv/main.tf
@@ -17,19 +17,85 @@ resource "aws_cloudfront_origin_access_control" "lysz210_cv_oac" {
   signing_protocol                  = "sigv4"
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "cv_lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "dummy_lambda_zip" {
+  type        = "zip"
+  output_path = "${path.module}/dummy_lambda.zip"
+  source {
+    content  = "exports.handler = async () => ({ statusCode: 200, body: 'Loading...' });"
+    filename = "index.js"
+  }
+}
+
+resource "aws_lambda_function" "nuxt_server" {
+  function_name = "lysz210CvServer"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs24.x"
+  filename      = data.archive_file.dummy.output_path
+  timeout       = 30
+
+  # Ignoriamo le modifiche al codice fatte da GitHub Actions
+  lifecycle {
+    ignore_changes = [filename, source_code_hash, last_modified]
+  }
+}
+
+resource "aws_lambda_function_url" "nuxt_url" {
+  function_name      = aws_lambda_function.nuxt_server.function_name
+  authorization_type = "NONE"
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
 resource "aws_cloudfront_distribution" "lysz210_cv_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
   aliases             = ["${var.domain_name}"]
 
+  # --- ORIGIN 1: Lambda (Server Side Rendering) ---
+  origin {
+    domain_name = split("/", aws_lambda_function_url.nuxt_url.function_url)[2]
+    origin_id   = "Lambda-lysz210-cv"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
   origin {
     domain_name              = aws_s3_bucket.lysz210_cv_storage.bucket_regional_domain_name
     origin_id                = "S3-lysz210-cv"
     origin_access_control_id = aws_cloudfront_origin_access_control.lysz210_cv_oac.id
   }
 
-  default_cache_behavior {
+  ordered_cache_behavior {
+    path_pattern     = "/_nuxt/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-lysz210-cv"
@@ -40,15 +106,20 @@ resource "aws_cloudfront_distribution" "lysz210_cv_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
-  custom_error_response {
-    error_code         = 404
-    response_code      = 200
-    response_page_path = "/index.html"
+  default_cache_behavior {
+    target_origin_id = "Lambda-lysz210-cv"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+
+    # Importante: Per SSR servono i Query Strings e spesso i Cookies
+    forwarded_values {
+      query_string = true
+      cookies { forward = "all" }
+    }
+    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
--- modules/cv/outputs.tf
+++ modules/cv/outputs.tf
@@ -14,6 +14,10 @@ output "cloudfront_id" {
   value = aws_cloudfront_distribution.lysz210_cv_distribution.id
 }
 
+output "lambda_function_name" {
+  value = aws_lambda_function.lysz210_cv_function.function_name
+}
+
 output "website_url" {
   value = "https://${aws_route53_record.lysz210_cv_alias.name}"
 }
